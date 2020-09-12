81b1475d37a511a535222606b5a35da6156402c7
Hyon Lee <hyonmail@gmail.com>
2020-09-12 13:26:15 -0700

    Fixed create delegation bug

--- app/controllers/delegations_controller.rb
+++ app/controllers/delegations_controller.rb
@@ -1,12 +1,12 @@
 class DelegationsController < ApplicationController
   def create
-    delegate = User.find(params[:delagate_id])
+    delegate = User.find(params['delegation']['delegate_id'])
 
     if delegate.nil?
       render json: { status: 404 } and return
     end
 
-    delegation = Delegation.create!(delegate: params[:delagate_id],
+    delegation = Delegation.create!(delegate: params['delegation']['delegate_id'],
                               user: @current_user)
 
     if delegation
