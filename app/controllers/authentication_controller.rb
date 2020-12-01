cfd1e20d56d61f5d82a825fd2161d4565a756606
Hyon Lee <hyonmail@gmail.com>
2020-11-30 19:27:11 -0800

    [CTY-12] Return username as part of /authenticate response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationController < ApplicationController
     command = AuthenticateUser.call(params[:name], params[:password])
 
     if command.success?
-      render json: { auth_token: command.result }
+      render json: { name: params[:name], auth_token: command.result }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
